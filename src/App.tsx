9547589c97db46cf71246d71f6fdf1718d3a4976
Karla Keurin <karlakeurinoliveira@gmail.com>
2024-06-04 02:30:48 -0300

    Adicionando configurações iniciais e header

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// import headerLogo from './assets/logo-img.svg';
+import './App.scss';
+
+function App() {
+  // const [count, setCount] = useState(0);
+  return (
+    <main>
+      <header className="header-container">
+        <span className="logo-text">Karla Keurin</span>
+        <nav className="menu-options">
+          <a>Sobre mim</a>
+          <a>Projetos</a>
+          <a>Contato</a>
+        </nav>
+      </header>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/linkedin-icon.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable max-len */
+export function LinkedInIcon() {
+  return (
+    <svg width="68" height="69" viewBox="0 0 68 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g filter="url(#filter0_d_133_892)">
+        <rect x="0.0290527" y="0.956055" width="64" height="64" rx="32" fill="#1733D2" />
+        <path d="M35.3624 47.956H28.029V25.956H35.3624V29.6227C36.9255 27.634 39.296 26.4484 41.8249 26.3905C46.3726 26.4158 50.0422 30.1166 50.029 34.6644V47.956H42.6957V35.581C42.4024 33.5325 40.6457 32.0126 38.5762 32.017C37.6711 32.0456 36.8166 32.442 36.2101 33.1145C35.6036 33.7871 35.2975 34.6778 35.3624 35.581V47.956ZM24.3624 47.956H17.0291V25.956H24.3624V47.956ZM20.6957 22.2894C18.6707 22.2894 17.0291 20.6478 17.0291 18.6227C17.0291 16.5977 18.6707 14.9561 20.6957 14.9561C22.7208 14.9561 24.3624 16.5977 24.3624 18.6227C24.3624 19.5952 23.9761 20.5278 23.2884 21.2154C22.6008 21.9031 21.6682 22.2894 20.6957 22.2894Z" fill="#F9F1DA" />
+      </g>
+      <defs>
+        <filter id="filter0_d_133_892" x="0.0290527" y="0.956055" width="67" height="68" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+          <feOffset dx="3" dy="4" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0.0901961 0 0 0 0 0.0901961 0 0 0 0 0.0901961 0 0 0 1 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_133_892" />
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_133_892" result="shape" />
+        </filter>
+      </defs>
+    </svg>
+
+  );
+}
