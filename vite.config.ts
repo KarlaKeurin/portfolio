46301562c7e326fd8387d6af06b7ca354d14fce7
KarlaKeurin <karlakeurinoliveira@gmail.com>
2025-05-08 12:27:27 -0300

    Ajustes nas configurações do gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: "/portfolio",
   test: {
     globals: true,
     environment: 'jsdom',
