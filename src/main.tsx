18e9f3b661f2f87c4f82d32642af749651c16952
KarlaKeurin <karlakeurinoliveira@gmail.com>
2025-05-08 12:40:13 -0300

    Alterando a main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <I18nextProvider i18n={i18n}>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
       <App />
     </BrowserRouter>
   </I18nextProvider>
